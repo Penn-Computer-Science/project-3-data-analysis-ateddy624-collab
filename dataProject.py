59c9962ac1d960fc915bb490e391d88366022424
ateddy624-collab <ateddy624@phm.k12.in.us>
2025-10-10 09:49:36 -0400

    .

--- dataProject.py
+++ dataProject.py
@@ -7,9 +7,14 @@ import matplotlib.pyplot as plt
 df=pd.read_csv('C:Documents\GitHub\project-3-data-analysis-ateddy624-collab\surveyData.csv')
 mySurveyData=pd.DataFrame(df)
 
-#print(mySurveyData.info())
+print(mySurveyData.info())
 #print(mySurveyData)
-
-print(round(mySurveyData.describe()))
-GPAvsExercise=mySurveyData.groupby('hours_exercising')['GPA'].mean()
-print(GPAvsExercise)
+iterator=1
+hours_exercising={"Time exercising" : "GPA"}
+for index, row in mySurveyData.iterrows():
+    iterator+=1
+    hours_exercising[float(mySurveyData[row]['hours_exercising'])] = (mySurveyData[row]['GPA'])
+print(hours_exercising)
+#print(round(mySurveyData.describe()))
+#GPAvsExercise=mySurveyData.groupby(hours_exercising)['GPA'].mean()
+#print(GPAvsExercise)
