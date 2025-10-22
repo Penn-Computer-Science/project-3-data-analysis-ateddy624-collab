6334e2d1f5d5832b3e37571c446be6a16a4462f3
ateddy624-collab <ateddy624@phm.k12.in.us>
2025-10-22 09:46:29 -0400

    .

--- dataProject.py
+++ dataProject.py
@@ -9,12 +9,37 @@ mySurveyData=pd.DataFrame(df)
 
 print(mySurveyData.info())
 #print(mySurveyData)
-iterator=1
+dataPoints= 28
 hours_exercising={"Time exercising" : "GPA"}
-for index, row in mySurveyData.iterrows():
-    iterator+=1
-    hours_exercising[float(mySurveyData[row]['hours_exercising'])] = (mySurveyData[row]['GPA'])
+
+print("Average GPA by year:")
+print(mySurveyData.groupby('hours_exercising')['GPA'].mean()) # Average GPA by year
+print("-_"*40)
+#This code was attempted but didn't work:
+#for index, row in mySurveyData.iterrows():
+    #hours_exercising[float(mySurveyData[row]['hours_exercising'])] = (mySurveyData[row]['GPA'])
+#for i in range(dataPoints):
+mySurveyData.groupby('hours_exercising')['GPA'].mean().plot(kind='bar', color='skyblue')
+plt.title("Hours Exercising vs. GPA")
+plt.xlabel("Hours Exercising")
+plt.ylabel("GPA")
+plt.tight_layout()
+plt.show()
+
+
+mySurveyData['hours_exercising'].plot(kind='hist', bins=4, color='purple', edgecolor='black')
+plt.title("GPA Distribution")
+plt.xlabel("GPA")
+plt.ylabel("Number of Students")
+plt.show()
 print(hours_exercising)
+
+
+plt.scatter(mySurveyData['hours_exercising'], mySurveyData['GPA'], alpha=0.6)
+plt.title("GPA vs. hours exercising")
+plt.xlabel("Hours exercising")
+plt.ylabel("GPA")
+plt.show()
 #print(round(mySurveyData.describe()))
 #GPAvsExercise=mySurveyData.groupby(hours_exercising)['GPA'].mean()
 #print(GPAvsExercise)
